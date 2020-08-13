5a5dd784caf3d15c617e59085dd40ce373b2f213
killshot13 <dmreh@outlook.com>
2020-08-13 06:31:41 -0500

    jun 2020

---
+++ JavaScript/expressionMatter.js
@@ -0,0 +1,15 @@
+/** 
+ * Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+ * In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+ **/
+
+function expressionMatter(a, b, c) {
+    let exp1 = (a * (b + c));
+    let exp2 = (a * b * c);
+    let exp3 = (a + b * c);
+    let exp4 = (a + b + c);
+    let exp5 = ((a + b) * c);
+    let res = Math.max(exp1, exp2, exp3, exp4, exp5)
+    return res;
+}
+return expressionMatter;
