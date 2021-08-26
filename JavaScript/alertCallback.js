6e61838dc2b22a84aacd4eab6609b2e9c24479d4
Michael R <dmreh@outlook.com>
2021-08-26 03:42:27 -0500

    Update JavaScript/alertCallback.js

---
+++ JavaScript/alertCallback.js
@@ -0,0 +1,13 @@
+
+        // TODO: Add hello code
+        function getMessage(name) {
+            return 'Hello, ' + name + '...';
+        }
+        const message = getMessage('Ornella');
+        document.write(message);
+
+        // TODO: Add setTimeout code
+        setTimeout(
+            () => { document.write('...Hello again!')},
+            2000
+        );
