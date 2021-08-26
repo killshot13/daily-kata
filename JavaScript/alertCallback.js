71b22239a2c77ce840ed8763891096f18222108e
Michael R <dmreh@outlook.com>
2021-08-26 03:45:09 -0500

    Update JavaScript/alertCallback.js

--- JavaScript/alertCallback.js
+++ JavaScript/alertCallback.js
@@ -1,13 +1,13 @@
+// TODO: Add hello code
+function getMessage(name) {
+  return 'Hello, ' + name + '...';
+  }
 
-        // TODO: Add hello code
-        function getMessage(name) {
-            return 'Hello, ' + name + '...';
-        }
-        const message = getMessage('Ornella');
-        document.write(message);
+	const message = getMessage('Ornella');
+    document.write(message);
 
-        // TODO: Add setTimeout code
-        setTimeout(
-            () => { document.write('...Hello again!')},
-            2000
-        );
+// TODO: Add setTimeout code
+  setTimeout(
+  	() => { document.write('...Hello again!')},
+      2000
+    );
